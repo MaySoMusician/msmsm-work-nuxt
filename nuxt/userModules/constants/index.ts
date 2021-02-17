14d1543a484142dda0971efd4d0d8492c35f0af5
MaySoMusician <maysomusician@gmail.com>
2021-02-18 02:07:54 +0900

    Update constants

--- nuxt/userModules/constants/index.ts
+++ nuxt/userModules/constants/index.ts
@@ -1,15 +1,14 @@
 export const websiteName = 'MaySoMusician の音楽スタジオ'
 export const websiteNameShort = 'MSMSM'
 export const websiteCopyright =
-  '<p>&copy; 2020 MaySoMusician このウェブサイトのソースコードは MIT License でライセンスされています。</p>'
-export const websiteDescription =
-  'MaySoMusician の音楽スタジオ'
+  '<p>&copy; 2020-2021 MaySoMusician このウェブサイトのソースコードは MIT License でライセンスされています。</p>'
+export const websiteDescription = 'MaySoMusician の音楽スタジオ'
 
 export const titlePerPage = {
   index: 'ホーム',
   programming: 'プログラミング',
   music: '音楽',
-  about: 'About',
+  about: 'プロフィール',
 }
 
 export const baseUrl = 'https://msmsm.work/'
