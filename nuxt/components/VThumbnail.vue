de06da88b708a49926093e8807e4ac820225b12a
MaySoMusician <maysomusician@gmail.com>
2021-02-24 01:35:18 +0900

    Add thumbnail base component

---
+++ nuxt/components/VThumbnail.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-img v-bind="$attrs" contain>
+    <template #placeholder>
+      <v-row
+        class="fill-height ma-0"
+        style="position: relative"
+        align="center"
+        justify="center"
+      >
+        <v-skeleton-loader v-if="$attrs.src" type="image" width="100%" />
+        <div v-else style="width: 100%">
+          <v-skeleton-loader boilerplate type="image" width="100%" />
+          <span
+            style="
+              position: absolute;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+              font-size: 0.7rem;
+            "
+            >NO IMAGE</span
+          >
+        </div>
+      </v-row>
+    </template>
+  </v-img>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped></style>
